7ccd7d6e2c103dea0a19ac2a3e61fb3e6fbc7931
Vance Palacio <vanceism7@gmail.com>
2020-02-02 20:27:15 -0800

    Use config generated by lsp-generate-settings from vscode config
    This is more sure that the config is working correctly. Also fiddled with
    automatic installation of ps lang server, but it seems that the self-installed
    version works a lot better for project detection

--- lsp-purescript.el
+++ lsp-purescript.el
@@ -24,7 +24,6 @@
 
 ;;; Code:
 (require 'lsp-mode)
-(require 'json)
 
 (defgroup lsp-purescript nil
   "LSP support for Purescript, using purescript analysis server."
@@ -32,196 +31,207 @@
   ;; :link '(url-link "https://github.com/purescript-lang/sdk/tree/master/pkg/analysis_server")
   )
 
-;; Config
-(defcustom lsp-purescript-pursExe "purs"
+;;; Config
+;;; Generated with lsp-generate-settings from vscode config
+(defcustom lsp-purescript-purs-exe "purs"
   "Location of purs executable (resolved wrt PATH)"
-  :type 'string
+  :type (quote string)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-useCombinedExe t
+(defcustom lsp-purescript-use-combined-exe t
   "Whether to use the new combined purs executable. This will default to true in the future then go away."
-  :type 'boolean
+  :type (quote boolean)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-pscIdeServerExe "psc-ide-server"
+(defcustom lsp-purescript-psc-ide-server-exe "psc-ide-server"
   "Location of legacy psc-ide-server executable (resolved wrt PATH)"
-  :type 'string
+  :type (quote string)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-pscIdePort nil
+(defcustom lsp-purescript-psc-ide-port nil
   "Port to use for purs IDE server (whether an existing server or to start a new one). By default a random port is chosen (or an existing port in .psc-ide-port if present), if this is specified no attempt will be made to select an alternative port on failure."
-  :type '(choice (const :tag "Default" nil)
-                 (integer))
+  :type (quote (repeat number))
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-autoStartPscIde t
-  "Whether to automatically start/connect to purs IDE server when editing a PureScript file (includes connecting to an existing running instance). If this is disabled, various features like autocomplete, tooltips, and other type info will not work until start command is run manually."
-  :type 'boolean
+(defcustom lsp-purescript-auto-start-psc-ide t
+  "Whether to automatically start/connect to purs IDE server when editing a PureScript file
+(includes connecting to an existing running instance). If this is disabled, various features like autocomplete, tooltips, and other type info will not work until start command is run manually."
+  :type (quote boolean)
   :group 'lsp-purescript)
 
-
-(defcustom lsp-purescript-packagePath "bower_components"
+(defcustom lsp-purescript-package-path "bower_components"
   "Path to installed packages. Will be used to control globs passed to IDE server for source locations.  Change requires IDE server restart."
-  :type 'string
+  :type (quote string)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-addPscPackageSources nil
-  "Whether to add psc-package sources to the globs passed to the IDE server for source locations (specifically the output of `psc-package sources`, if this is a psc-package project). Update due to adding packages/changing package set requires psc-ide server restart."
-  :type 'boolean
+(defcustom lsp-purescript-add-psc-package-sources nil
+  "Whether to add psc-package sources to the globs passed to the IDE server for source locations
+(specifically the output of `psc-package sources`, if this is a psc-package project). Update due to adding packages/changing package set requires psc-ide server restart."
+  :type (quote boolean)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-addSpagoSources nil
-  "Whether to add spago sources to the globs passed to the IDE server for source locations (specifically the output of `spago sources`, if this is a spago project). Update due to adding packages/changing package set requires psc-ide server restart."
-  :type 'boolean
+(defcustom lsp-purescript-add-spago-sources nil
+  "Whether to add spago sources to the globs passed to the IDE server for source locations
+(specifically the output of `spago sources`, if this is a spago project). Update due to adding packages/changing package set requires psc-ide server restart."
+  :type (quote boolean)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-sourcePath "src"
+(defcustom lsp-purescript-source-path "src"
   "Path to application source root. Will be used to control globs passed to IDE server for source locations. Change requires IDE server restart."
- :type 'string
- :group 'lsp-purescript)
+:type (quote string)
+  :group 'lsp-purescript)
 
-(defcustom lsp-purescript-buildCommand "pulp build -- --json-errors"
+(defcustom lsp-purescript-build-command "pulp build -- --json-errors"
   "Build command to use with arguments. Not passed to shell. eg `pulp build -- --json-errors` (this default requires pulp >=10)"
-  :type 'string
+  :type (quote string)
   :group 'lsp-purescript)
 
-
-(defcustom lsp-purescript-fastRebuild t
+(defcustom lsp-purescript-fast-rebuild t
   "Enable purs IDE server fast rebuild"
-  :type 'boolean
+  :type (quote boolean)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-censorWarnings nil
+(defcustom lsp-purescript-censor-warnings nil
   "The warning codes to censor, both for fast rebuild and a full build. Unrelated to any psa setup. e.g.: [\"ShadowedName\",\"MissingTypeDeclaration\"]"
-  :type '(repeat string)
+  :type (quote lsp-string-vector)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-autocompleteAllModules t
+(defcustom lsp-purescript-autocomplete-all-modules t
   "Whether to always autocomplete from all built modules, or just those imported in the file. Suggestions from all modules always available by explicitly triggering autocomplete."
-  :type 'boolean
+  :type (quote boolean)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-autocompleteAddImport t
+(defcustom lsp-purescript-autocomplete-add-import t
   "Whether to automatically add imported identifiers when accepting autocomplete result."
-  :type 'boolean
+  :type (quote boolean)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-autocompleteLimit nil
+(defcustom lsp-purescript-autocomplete-limit nil
   "Maximum number of results to fetch for an autocompletion request. May improve performance on large projects."
-  :type '(choice (const nil)
-                 integer)
+  :type (quote (repeat nil))
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-autocompleteGrouped t
+(defcustom lsp-purescript-autocomplete-grouped t
   "Whether to group completions in autocomplete results. Requires compiler 0.11.6"
-  :type 'boolean
+  :type (quote boolean)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-importsPreferredModules "Prelude"
+(defcustom lsp-purescript-imports-preferred-modules ["Prelude"]
   "Module to prefer to insert when adding imports which have been re-exported. In order of preference, most preferred first."
-  :type '(repeat string)
+  :type (quote lsp-string-vector)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-preludeModule "Prelude"
+(defcustom lsp-purescript-prelude-module "Prelude"
   "Module to consider as your default prelude, if an auto-complete suggestion comes from this module it will be imported unqualified."
-  :type 'string
+  :type (quote string)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-addNpmPath nil
+(defcustom lsp-purescript-add-npm-path nil
   "Whether to add the local npm bin directory to the PATH for purs IDE server and build command."
-  :type 'boolean
+  :type (quote boolean)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-pscIdelogLevel ""
+(defcustom lsp-purescript-psc-idelog-level ""
   "Log level for purs IDE server"
-  :type 'string
+  :type (quote string)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-editorMode nil
+(defcustom lsp-purescript-editor-mode nil
   "Whether to set the editor-mode flag on the IDE server"
-  :type 'boolean
+  :type (quote boolean)
   :group 'lsp-purescript)
 
 (defcustom lsp-purescript-polling nil
   "Whether to set the polling flag on the IDE server"
-  :type 'boolean
+  :type (quote boolean)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-outputDirectory nil
+(defcustom lsp-purescript-output-directory nil
   "Override purs ide output directory (output/ if not specified). This should match up to your build command"
-  :type 'string
+  :type (quote string)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-trace.server 'off
+(defcustom lsp-purescript-trace-server "off"
   "Traces the communication between VSCode and the PureScript language service."
-  :type '(choice (const "off")
-                 (const "messages")
-                 (const "verbose"))
+  :type (quote (choice (:tag "off" "messages" "verbose")))
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-codegenTargets nil
+(defcustom lsp-purescript-codegen-targets nil
   "List of codegen targets to pass to the compiler for rebuild. e.g. js, corefn. If not specified (rather than empty array) this will not be passed and the compiler will default to js. Requires 0.12.1+"
-  :type '(repeat string)
+  :type (quote lsp-string-vector)
   :group 'lsp-purescript)
 
-(defcustom lsp-purescript-useNpx t
+(lsp-register-custom-settings
+ (quote
+  (("purescript.codegenTargets" lsp-purescript-codegen-targets)
+   ("purescript.trace.server" lsp-purescript-trace-server)
+   ("purescript.outputDirectory" lsp-purescript-output-directory)
+   ("purescript.polling" lsp-purescript-polling t)
+   ("purescript.editorMode" lsp-purescript-editor-mode t)
+   ("purescript.pscIdelogLevel" lsp-purescript-psc-idelog-level)
+   ("purescript.addNpmPath" lsp-purescript-add-npm-path t)
+   ("purescript.preludeModule" lsp-purescript-prelude-module)
+   ("purescript.importsPreferredModules" lsp-purescript-imports-preferred-modules)
+   ("purescript.autocompleteGrouped" lsp-purescript-autocomplete-grouped t)
+   ("purescript.autocompleteLimit" lsp-purescript-autocomplete-limit)
+   ("purescript.autocompleteAddImport" lsp-purescript-autocomplete-add-import t)
+   ("purescript.autocompleteAllModules" lsp-purescript-autocomplete-all-modules t)
+   ("purescript.censorWarnings" lsp-purescript-censor-warnings)
+   ("purescript.fastRebuild" lsp-purescript-fast-rebuild t)
+   ("purescript.buildCommand" lsp-purescript-build-command)
+   ("purescript.sourcePath" lsp-purescript-source-path)
+   ("purescript.addSpagoSources" lsp-purescript-add-spago-sources t)
+   ("purescript.addPscPackageSources" lsp-purescript-add-psc-package-sources t)
+   ("purescript.packagePath" lsp-purescript-package-path)
+   ("purescript.autoStartPscIde" lsp-purescript-auto-start-psc-ide t)
+   ("purescript.pscIdePort" lsp-purescript-psc-ide-port)
+   ("purescript.pscIdeServerExe" lsp-purescript-psc-ide-server-exe)
+   ("purescript.useCombinedExe" lsp-purescript-use-combined-exe t)
+   ("purescript.pursExe" lsp-purescript-purs-exe))))
+
+;;;
+;;; Server Startup Settings
+;;;
+;; (lsp-dependency 'purescript-language-server
+;;                 '(:system "purescript-language-server")
+;;                 '(:npm :package "purescript-language-server"
+;;                        :path "purescript-language-server"))
+
+;; Download language server automatically
+;; Unused for now since a self-installed purescript-language-server detects project paths better
+;; (defun lsp-purescript--server-command ()
+;;   "Generate LSP startup command."
+;;   (cons
+;;    (lsp-package-path 'purescript-language-server)
+;;    '("--stdio")))
+
+(defcustom lsp-purescript-use-npx t
   "Whether to execute purescript-language-server using npx or a globally installed version. Defaults to Npx"
   :type '(choice (const :tag "Npx" t)
                  (const :tag "Global" nil))
   :group 'lsp-purescript)
 
-;; ----------
-;; Main stuff
-;; ----------
+
 (defun lsp-purescript--server-command ()
   "Generate LSP startup command."
-  (if lsp-purescript-useNpx
-      (append '("npx" "purescript-language-server" "--stdio" "--config") (generate-json-config))
-    (append '("purescript-language-server" "--stdio" "--config") (generate-json-config))))
-
-(lsp-purescript--server-command)
-(generate-json-config)
-
-(defun generate-json-config ()
-  "Generate json config for purescript language server executable from custom options"
-  (list
-   (let (obj (json-new-object))
-     (setf obj (json-add-to-object obj "addNpmPath" lsp-purescript-addNpmPath))
-     (setf obj (json-add-to-object obj "pursExe" lsp-purescript-pursExe))
-     (setf obj (json-add-to-object obj "useCombinedExe" lsp-purescript-useCombinedExe))
-     (setf obj (json-add-to-object obj "pscIdeServerExe" lsp-purescript-pscIdeServerExe))
-     (setf obj (json-add-to-object obj "pscIdePort" lsp-purescript-pscIdePort))
-     (setf obj (json-add-to-object obj "autoStartPscIde" lsp-purescript-autoStartPscIde))
-     (setf obj (json-add-to-object obj "packagePath" lsp-purescript-packagePath))
-     (setf obj (json-add-to-object obj "addPscPackageSources" lsp-purescript-addPscPackageSources))
-     (setf obj (json-add-to-object obj "addSpagoSources" lsp-purescript-addSpagoSources))
-     (setf obj (json-add-to-object obj "sourcePath" lsp-purescript-sourcePath))
-     (setf obj (json-add-to-object obj "buildCommand" lsp-purescript-buildCommand))
-     (setf obj (json-add-to-object obj "fastRebuild" lsp-purescript-fastRebuild))
-     (setf obj (json-add-to-object obj "censorWarnings" lsp-purescript-censorWarnings))
-     (setf obj (json-add-to-object obj "autocompleteAllModules" lsp-purescript-autocompleteAllModules))
-     (setf obj (json-add-to-object obj "autocompleteAddImport" lsp-purescript-autocompleteAddImport))
-     (setf obj (json-add-to-object obj "autocompleteLimit" lsp-purescript-autocompleteLimit))
-     (setf obj (json-add-to-object obj "autocompleteGrouped" lsp-purescript-autocompleteGrouped))
-     (setf obj (json-add-to-object obj "importsPreferredModules" lsp-purescript-importsPreferredModules))
-     (setf obj (json-add-to-object obj "preludeModule" lsp-purescript-preludeModule))
-     (setf obj (json-add-to-object obj "addNpmPath" lsp-purescript-addNpmPath))
-     (setf obj (json-add-to-object obj "pscIdelogLevel" lsp-purescript-pscIdelogLevel))
-     (setf obj (json-add-to-object obj "editorMode" lsp-purescript-editorMode))
-     (setf obj (json-add-to-object obj "polling" lsp-purescript-polling))
-     (setf obj (json-add-to-object obj "outputDirectory" lsp-purescript-outputDirectory))
-     (setf obj (json-add-to-object obj "trace.server" lsp-purescript-trace.server))
-     (setf obj (json-add-to-object obj "codegenTargets" lsp-purescript-codegenTargets))
-     (json-encode (json-add-to-object obj "useNpx" lsp-purescript-useNpx)))))
+  (if lsp-purescript-use-npx
+      '("npx" "purescript-language-server" "--stdio")
+    '("purescript-language-server" "--stdio")))
 
 (lsp-register-client
  (make-lsp-client :new-connection (lsp-stdio-connection (lsp-purescript--server-command))
                   :major-modes '(purescript-mode)
                   :priority -1
-                  ;; :initialization-options
-                  ;; `((onlyAnalyzeProjectsWithOpenFiles . ,lsp-purescript-only-analyze-projects-with-open-files)
-                  ;;   (suggestFromUnimportedLibraries . ,lsp-purescript-suggest-from-unimported-libraries))
-                  :server-id 'purescript_language_server))
+                  :initialized-fn (lambda (workspace)
+                                    (with-lsp-workspace workspace
+                                      (lsp--set-configuration
+                                       (lsp-configuration-section "purescript"))))))
+                  ;; :server-id 'purescript_language_server
+                  ;; :download-server-fn (lambda (_client callback error-callback _update?)
+                  ;;                       (lsp-package-ensure
+                  ;;                        'purescript-language-server
+                  ;;                        callback
+                  ;;                        error-callback))))
 
 (provide 'lsp-purescript)
 
